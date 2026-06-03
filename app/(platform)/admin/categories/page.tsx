b04d643387b96f023d712fb1eec1f7b0b8d9caca
JohnyAls <techevo404@gmail.com>
2026-06-04 00:39:12 +0300

    rf code:nextjs dynamic prerendering settings

--- app/(platform)/admin/categories/page.tsx
+++ app/(platform)/admin/categories/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { FormEvent, useState } from "react";
 
--- app/(platform)/admin/content/page.tsx
+++ app/(platform)/admin/content/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 import { useState } from "react";
