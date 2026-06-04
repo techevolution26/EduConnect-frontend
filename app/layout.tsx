61916714b71cfff30f4eb62fc5237440249e2f5a
JohnyAls <techevo404@gmail.com>
2026-06-04 21:40:57 +0300

    adding: verce-analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import{Analytics} from "@vercel/analytics/react";
 
 import Providers from "@/app/providers";
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
